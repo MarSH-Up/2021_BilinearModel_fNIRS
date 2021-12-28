10fba711353898bc681591fe64671a900c3bd23b
Mario De Los Santos <64880712+MarSH-Up@users.noreply.github.com>
2021-12-28 14:08:52 -0600

    Update BilinearModel_Testing_M DLS.m

--- Euler-Method/BilinearModel_Testing_M DLS.m
+++ Euler-Method/BilinearModel_Testing_M DLS.m
@@ -2,7 +2,7 @@
 % Authors: Mario de los Santos, Felipe Orihuela-Espina, Javier Herrara-Vega
 %
 %% Log:
-% October, 2021: MDLS
+% December, 2021: MDLS
 %   
 %%
 clear; 
@@ -21,45 +21,53 @@ B(:,:,2) = B2;
 C = [0.08 0; 0.06 0];
 
 %Sample Frequency. This version use the Frequency as H integration step for Euler ODE Method.
-freq = 10.4;
+freq = [1 10 20 50 100 1000];
+%for i = 1:6
 %Step for Euler ODE Method H
-    step = 1/freq;
+    step = 1/freq(2);
     %%
     %Stimulus train
-    [U_stimulus, timestamps] = BilinearModel_StimulusTrainGenerator(freq, 5, 25, 2); 
+    [U_stimulus, timestamps] = BilinearModel_StimulusTrainGenerator(freq(2), 5, 25, 2); 
     %%
     %Neurodynamics
-    [Z] = BilinearModel_Neurodynamics_Z(A,B,C, U_stimulus, step);
+    [Z] = BilinearModel_Neurodynamics_B(A,B,C, U_stimulus, step);
     %%
     %HEMODYNAMICS
+    %P_SD = [[0.0775;-0.0087] [-0.1066;0.0299] [0.0440;-0.0129] [0.8043;-0.7577]]; %Based on SPM
     P_SD = [[0;0.05] [0;0.05] [0;0.05] [0;1]];
-    [Sj,Vj,qj,pj] = BilinearModel_Hemodynamics_Naive_v2(Z, U_stimulus, P_SD, A, step);
+    [qj,pj] = BilinearModel_Hemodynamics_Naive_v2(Z, U_stimulus, P_SD, A, step);
     %%
     %OPTICS
-    Noise = 0.4;
-    [Y] = BilinearModel_Optics_Naive(pj, qj,U_stimulus,A, Noise);
+    Noise = 0;
+    %[Y] = BilinearModel_Optics_Naive(pj, qj,U_stimulus,A, Noise);
+    [Y] = BilinearModel_Optics_Naive_v2(pj, qj,U_stimulus,A, Noise);
     %%
     %Just plotting
     figure;
-    subplot(211);
-    plot(timestamps,U_stimulus(1,:),'LineWidth',2);
+    subplot(2,1,1);
+    plot(timestamps,U_stimulus(1,:),'LineWidth',4);
     xlabel('Time (s)');
     ylabel('Task (U_{1})');
-    subplot(212);
-    plot(timestamps,U_stimulus(2,:),'LineWidth',2);
+    subplot(2,1,2);
+    plot(timestamps,U_stimulus(2,:),'LineWidth',4);
     xlabel('Time (s)');
     ylabel('Task (U_{2})');
     title('Stimulus');
+    
     Zm1  = Z(1,:);
-    Zsma = Z(2,:);  
+    Zsma = Z(2,:); 
+    
     figure
     ax1 = subplot(2,1,1); % top subplot
-    plot(ax1,timestamps,Zm1,'LineWidth',2);
+    plot(ax1,timestamps,Zm1,'LineWidth',4);
     title(ax1,'Zm1');
     xlabel(ax1,'Time');
     ax2 = subplot(2,1,2); % bottom subplot
-    plot(ax2,timestamps,Zsma,'LineWidth',2);
+    plot(ax2,timestamps,Zsma,'LineWidth',4);
     title(ax2,'Zsma');
     xlabel(ax2,'Time');
+    disp(step)
+
+%end 
 
     
